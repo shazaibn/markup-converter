1cd2c4d25e27e1062d0005606858111b60d47a05
Jarrod Overson <jsoverson@gmail.com>
2022-08-12 12:25:19 -0400

    generalized new() constructor to take a Format, moved old new() to from_path()

--- examples/jaq.rs
+++ examples/jaq.rs
@@ -2,7 +2,7 @@ use jaq_core::{parse, Ctx, Definitions, Val};
 use markup_converter::Transcoder;
 
 fn main() -> anyhow::Result<()> {
-    let input = Transcoder::new("tests/test.yaml")?.to_json()?;
+    let input = Transcoder::from_path("tests/test.yaml")?.to_json()?;
 
     let filter = ".name";
 
@@ -13,7 +13,7 @@ fn main() -> anyhow::Result<()> {
 
     // parse the filter in the context of the given definitions
     let mut errs = Vec::new();
-    let f = parse::parse(&filter, parse::main()).0.unwrap();
+    let f = parse::parse(filter, parse::main()).0.unwrap();
     let f = defs.finish(f, Vec::new(), &mut errs);
 
     // iterator over the output values
